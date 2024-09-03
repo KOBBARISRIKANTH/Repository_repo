4052eab71591205614a8891aaee90fecbf26ea66
KOBBARISRIKANTH <168889121+KOBBARISRIKANTH@users.noreply.github.com>
2024-09-03 12:42:46 +0530

    Add files via upload

---
+++ Lab3.py
@@ -0,0 +1,6 @@
+emp={"ename":["Sriaknth","kesava","Avinash","sai","Raj"],"Age":[23,34,45,32,54],"Salary":[65000,23000,27000,14000,19000]}
+l1=emp["Salary"]
+print("Total salary is:",sum(l1))
+print("maximum salary of the employee:",max(l1))
+print("maximum salary of the employee:",min(l1))
+
